fb7b6029a953c481609a4f505a3c42492d648615
jatin77 <clockstopfountain@gmail.com>
2019-07-18 07:42:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>contact form</title>
+</head>
+<body>
+
+  <main>
+    <p>SEND E-MAIL</p>
+    <form action="contactform.php" method="post">
+      <input type="text" name="name" placeholder="Name">
+      <input type="text" name="email" placeholder="Email">
+      <button type="submit" name="submit">SEND EMAIL</button>
+    </form>
+  </main>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Document</title>
+  </head>
+  <body>
+    <form method="POST">
+      <p>
+        <b>Your Name:</b> <input value="value" type="text" name="name" /><br />
+
+        <b>E-mail:</b> <input type="text" name="email" /><br />
+      </p>
+
+      <p><input type="submit" value="Send it!" /></p>
+    </form>
+  </body>
+</html> -->
